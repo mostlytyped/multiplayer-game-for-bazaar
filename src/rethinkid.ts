56dd2ec4145378ed9cd0bb3f786121579604aadc
Paddy <ohanlon.paddy@gmail.com>
2022-10-28 18:07:07 +0200

    comment out local dev config

--- src/rethinkid.ts
+++ src/rethinkid.ts
@@ -6,8 +6,8 @@ export const rid = new RethinkID({
   appId: process.env.VUE_APP_APP_ID,
   loginRedirectUri: window.location.origin,
   // Enable local dev
-  oAuthUri: "http://localhost:4444",
-  dataApiUri: "http://localhost:4000",
+  // oAuthUri: "http://localhost:4444",
+  // dataApiUri: "http://localhost:4000",
   dataAPIConnectErrorCallback: function (e: any) {
     // this = RethinkID
     // @ts-ignore
