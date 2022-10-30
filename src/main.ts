035229585b9c1027ab9f1d545e8e760ca6240a3a
Paddy <ohanlon.paddy@gmail.com>
2022-10-30 07:11:47 +0100

    Console log node_env

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
 createApp(App).use(store).use(router).mount("#app");
